e0604ee5acf328ad8d8b5b298116721ffde2646d
Inadati <drumake.unr@gmail.com>
2020-04-15 12:58:36 +0900

    scraping-bot コンテナ化

--- scraping-bot/script/settings.py
+++ scraping-bot/script/settings.py
@@ -1,10 +1,10 @@
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
-load_dotenv("../../.env")
+# load_dotenv(verbose=True)
+# load_dotenv("../../.env")
 
 DL_FILE = "https://web.pref.hyogo.lg.jp/kk03/documents/yousei.xlsx"
 DL_FILE_NAME = "covid19-file.xlsx"
-# DB_CONNECT = os.environ.get("DB_CONNECT").strip("\"")
-DB_CONNECT = "host=localhost port=5432 user=postgres dbname=postgres password=postgres sslmode=disable"
+DB_CONNECT = os.environ.get("DB_CONNECT").strip("\"")
+# DB_CONNECT = "host=localhost port=5432 user=postgres dbname=postgres password=postgres sslmode=disable"
